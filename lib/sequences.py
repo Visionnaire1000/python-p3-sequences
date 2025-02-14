e79a1f758cc5f721e559cba2f12bc90d9fef4418
Louis <cheruiyotlouis@gmail.com>
2025-02-14 21:24:53 +0300

    Initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])
+        return
+
+    fibonacci = [0, 1] if n > 1 else [0]
+    
+    for i in range(2, n):
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+
+    print(fibonacci)
+
+# Example usage:
+print_fibonacci(10)
